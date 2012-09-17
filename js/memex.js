bd2b50fef9feeff8b03054f70fef3b0e98a73e27
Max Gutman <mxgutman@gmail.com>
2012-09-17 13:28:14 -0700

    adding base code

---
+++ js/memex.js
@@ -0,0 +1,21 @@
+$(function() {
+
+    /* define variables */
+
+    Memex = {
+        // initialize event handler: clicks and keypress
+        init: function() {
+
+        },
+
+        render: function() {
+
+        }
+
+     };
+
+     /* initialize and render for the first time */
+     Memex.init();
+     Memex.render();
+});
+
